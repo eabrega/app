b1dc64e326b6bf00627e587992fc54c6ff4b130d
Evgeny Brega <eabrega@gmail.com>
2021-06-11 16:55:30 +0300

    build

---
+++ dist/precache-manifest.a2fe11b0aab091ad00e2dc83018f7df8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a368ddd9f14ad917cf2",
+    "url": "/css/app.96a0a0d3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5ac94eec4c7312cce44a8a4f5b7a60ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a368ddd9f14ad917cf2",
+    "url": "/js/app.d97a1030.js"
+  },
+  {
+    "revision": "637bb7981fb669bf1465",
+    "url": "/js/chunk-vendors.e4f57f5c.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "75179c735fa081ee90eb75144a205baa",
+    "url": "/media/00225.mp3"
+  },
+  {
+    "revision": "f6a536ddac80b38e4842a1e159e36956",
+    "url": "/media/01426.mp3"
+  },
+  {
+    "revision": "bd8bf4410ca387f594cd586a8623a5a0",
+    "url": "/media/02019.mp3"
+  },
+  {
+    "revision": "a896f869eebc3858bdd49a0d3f862376",
+    "url": "/media/02036.mp3"
+  },
+  {
+    "revision": "12b4281f572053a2a0594c180a73b9ae",
+    "url": "/media/03912.mp3"
+  },
+  {
+    "revision": "d0fabc0dc8f847a422f837c1f7a39478",
+    "url": "/media/04457.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
